98e7b58414f36a9a41732bd74ccd2ac395f668b5
liuderchi <liuderchi@gmail.com>
2015-11-24 13:27:24 +0800

    feat: Pass all test in tournament_test.py. Code contains comment

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
-# 
-# tournament.py -- implementation of a Swiss-system tournament
-#
+"""tournament.py
+
+Created Date: 2015-11-05
+Arthur: Derek Liu
+Description: UDND P2: implementation of a Swiss-system tournament
+"""
 
 import psycopg2
 
@@ -13,25 +16,48 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("DELETE from matches;")
+    db.commit()
+    db.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("DELETE from players;")
+    cursor.execute("DELETE from standings;")
+    db.commit()
+    db.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("select * from players;")
+    results = cursor.fetchall()
+    db.close()
+    return  len(results)
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO players ( name ) VALUES ( %s );" , (name,) ) #avoid injection
+    cursor.execute("INSERT INTO standings ( name, wins, matches ) VALUES ( %s, %s, %s );" ,(name, 0, 0)  )
+    db.commit()
+    db.close()
 
 
 def playerStandings():
@@ -47,6 +73,14 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    db = connect()
+    cursor = db.cursor()
+    my_query = "SELECT * FROM standings ORDER BY wins DESC;"
+    cursor.execute(my_query)
+    results = cursor.fetchall()
+    db.close()
+    #for sub in results:  print(sub);
+    return results
 
 
 def reportMatch(winner, loser):
@@ -56,16 +90,44 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+    db = connect()
+    csr = db.cursor()
+
+    #update standings: winner
+    csr.execute("SELECT pid,name,wins,matches FROM standings WHERE pid=%s;",
+                 (winner,))
+    results = csr.fetchall()
+    wins = results[0][2] + 1
+    matches = results[0][3] + 1
+    csr.execute("UPDATE standings set wins=%s, matches=%s where pid=%s;",
+                (wins, matches, winner))
+
+    #update standings: loser
+    csr.execute("SELECT pid,name,wins,matches FROM standings WHERE pid=%s;", (loser,))
+    results = csr.fetchall()
+    matches = results[0][3] + 1
+    csr.execute("UPDATE standings set matches=%s where pid=%s;", (matches, loser))
+
+    #add a match
+    csr.execute("SELECT pid,name FROM standings WHERE pid=%s;", (loser,))
+    loser_name = csr.fetchall()[0][1]
+    csr.execute("SELECT pid,name FROM standings WHERE pid=%s;", (winner,))
+    winner_name = csr.fetchall()[0][1]
+    csr.execute("INSERT INTO matches (winner, loser) VALUES ( %s, %s);",
+                (winner_name, loser_name))
+
+    db.commit()
+    db.close()
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +135,13 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
-
+    pairings = []
+    standings = playerStandings()
+    #[ ( pid SERIAL primary key, name TEXT, wins SMALLINT, matches SMALLINT ) ]
+    #for itr in standings:        print ( itr );
+    while len(standings) > 0:
+        player1 = standings.pop(0)
+        player2 = standings.pop(0)
+        pairings[ len(pairings):] = [ (player1[0], player1[1], player2[0], player2[1]) ]
+    #for itr in pairings:        print ( itr );
+    return pairings
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,30 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- 1. create db and connect to db
+-- reset DB
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+\c tournament;
 
+-- 2. create tables and insert data
+CREATE TABLE players ( pid SERIAL primary key, name TEXT );
+CREATE TABLE standings ( pid SERIAL primary key, name TEXT, wins SMALLINT, matches SMALLINT );
+CREATE TABLE matches ( mid SERIAL primary key, winner TEXT, loser TEXT );
+
+--CREATE TABLE posts ( content TEXT, time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, id SERIAL );
+--INSERT INTO players (name) VALUES ( 'Kobe');
+--INSERT INTO players (name) VALUES ( 'Paul');
+--INSERT INTO matches (winner, loser) VALUES ( 'Paul','Kobe');
+--INSERT INTO matches (winner, loser) VALUES ( 'Paul', 'Curry');
+--INSERT INTO standings (name, wins, matches) VALUES ( 'Paul', 0, 20 );
+
+-- 3. create views
+--CREATE VIEW PlayerNamesView AS
+--SELECT name FROM players;
+--CREATE VIEW MatchNamesView AS
+--SELECT winner, loser FROM matches;
+--SELECT pid,name,wins,matches FROM standings WHERE name='Paul';
+
+-- 4. some Application query
+--SELECT loser FROM MatchNamesView;
